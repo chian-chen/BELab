b0a56e7ecd77e0636660927c7186c047acadf8c6
chian-chen <64936325+chian-chen@users.noreply.github.com>
2023-05-28 15:46:30 +0800

    delete .DS_Store

--- delete.py
+++ delete.py
@@ -1,13 +1,30 @@
+# import os
+
+# # Define the root directory
+# root_directory = './data/'
+
+# # Recursively iterate through all directories and files
+# for root, dirs, files in os.walk(root_directory):
+#     for file in files:
+#         # Check if the file name does not contain 'new'
+#         if 'new' not in file:
+#             # Construct the full file path
+#             file_path = os.path.join(root, file)
+            
+#             # Delete the file
+#             os.remove(file_path)
+#             print(f"Deleted file: {file_path}")
+
 import os
 
 # Define the root directory
-root_directory = './data/'
+root_directory = './'
 
 # Recursively iterate through all directories and files
 for root, dirs, files in os.walk(root_directory):
     for file in files:
-        # Check if the file name does not contain 'new'
-        if 'new' not in file:
+        # Check if the file is .DS_Store
+        if file == '.DS_Store':
             # Construct the full file path
             file_path = os.path.join(root, file)
             
